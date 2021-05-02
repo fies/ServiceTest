7138081bb17e4b42a99abd03dd2a57e9bb6d586e
fies <esfikries@gmail.com>
2021-05-02 23:28:43 +0300

    Create MainTest2.java

---
+++ src/main/java/MainTest2.java
@@ -0,0 +1,17 @@
+public class MainTest2 {
+    public static void main(String[] args) {
+         System.out.println("master version 1");
+         System.out.println("master version 1");
+        System.out.println("master version 2");
+        System.out.println("master version 3");
+        System.out.println("master version 4");
+
+    }
+      public  void test() {
+         System.out.println("master version 1");
+         System.out.println("master version 1");
+        System.out.println("master version 1");
+          System.out.println("master version 1");
+
+    }
+}
