3bbaa848fddb1f5d7618e93c1846184fb0480dc2
fies <esfikries@gmail.com>
2021-05-02 23:27:04 +0300

    Update MainTest.java

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,14 +1,17 @@
 public class MainTest {
     public static void main(String[] args) {
-
-        System.out.println("master version 1");
-         System.out.println("master version 1");
-         System.out.println("master version 1");
          System.out.println("master version 1");
          System.out.println("master version 1");
         System.out.println("master version 2");
         System.out.println("master version 3");
         System.out.println("master version 4");
+
+    }
+      public  void test() {
+         System.out.println("master version 1");
+         System.out.println("master version 1");
+        System.out.println("master version 1");
+          System.out.println("master version 1");
 
     }
 }
