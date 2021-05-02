50c34af10b8109a3345d77f083b7046c130b7d64
fies <esfikries@gmail.com>
2021-05-02 23:14:00 +0300

    Update MainTest.java

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,6 +2,10 @@ public class MainTest {
     public static void main(String[] args) {
 
         System.out.println("master version 1");
+         System.out.println("master version 1");
+         System.out.println("master version 1");
+         System.out.println("master version 1");
+         System.out.println("master version 1");
         System.out.println("master version 2");
         System.out.println("master version 3");
         System.out.println("master version 4");
